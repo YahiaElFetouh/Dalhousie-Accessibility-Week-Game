e5a64b1153bccb428161dcfe17e515e37e017d2f
Mac Burton <macburton1000@gmail.com>
2022-11-07 01:23:11 -0400

    Renamed variables

---
+++ Assets/Scripts/Scene Transition/TransitionSliderInput.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionSliderInput : MonoBehaviour
+{
+    public GameObject transition;
+    private bool transitionActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transitionActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!transitionActive)
+        {
+            if (Input.GetKeyDown(KeyCode.E) && !transition.GetComponent<MoveSlider>().enabled)
+            {
+                transition.GetComponent<MoveSlider>().ResetVars();
+                transition.GetComponent<MoveSlider>().enabled = true;
+            }
+            if (transition.GetComponent<MoveSlider>().IsFinishedLerp())
+            {
+                transition.GetComponent<MoveSlider>().enabled = false;
+                transitionActive = true;
+            }
+        }
+    }
+
+}
