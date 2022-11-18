ab0c7526340e6cfb94e4e5ccc7f589f4180d9972
Joel Kuruvilla <jl567056@dal.ca>
2022-11-18 02:03:12 -0400

    Readding Missing Items from Development

---
+++ Assets/Scripts/MovePointer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePointer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+            transform.position = target;
+        }
+    }
+}
