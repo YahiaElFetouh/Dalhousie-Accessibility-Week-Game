f0a8f9cc1044617488ea75104f6871ad2fbdebe8
Joel Kuruvilla <jl567056@dal.ca>
2022-11-28 19:49:48 -0400

    Added Scene Connection routes for all scenerios

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,12 +4,35 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour{
+
+
+   //Play the game from beginning to end
    public void PlayGame(){
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+      Debug.Log("PLAY!");
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
    }
 
+
+   //Play the game from selection screen
+   public void LoadADHDScenerio(){
+      Debug.Log("ADHD story loading");
+      SceneManager.LoadScene("(1) ADHD Story");
+   }
+   public void LoadSubstanceAbuseScenerio(){
+      Debug.Log("Substance Use story loading");
+      SceneManager.LoadScene("(2) Substance Use Story");
+   }
+   public void LoadBipolarScenerio(){
+      Debug.Log("Bi-polar story loading");
+      SceneManager.LoadScene("(3) Bi-polar Story");
+   }
+
+
+
+   //Exit Game
    public void QuitGame(){
     Debug.Log("QUIT");
     Application.Quit();
    }
 }
+
