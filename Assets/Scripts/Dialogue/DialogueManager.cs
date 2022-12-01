2a92cdb7f835103461976f920329d4154cf3876a
unknown <bn439251@dal.ca>
2022-12-01 11:49:07 -0400

    applying the typewriter to the dialogueBox

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -13,6 +13,8 @@ public class DialogueManager : MonoBehaviour
     [SerializeField] private GameObject dialoguePanel;
     [SerializeField] private TextMeshProUGUI dialogueText;
 
+    [SerializeField] private float typeWriterSpeed = 30f;
+
 
     [Header("Choices UI")]
     [SerializeField] private GameObject[] choices;
@@ -23,22 +25,24 @@ public class DialogueManager : MonoBehaviour
 
     private void Awake()
     {
-        
+
 
         if (instance != null)
         {
             Debug.LogWarning("Found more than one Dialogue Manager in the Scene");
         }
         instance = this;
-    } 
+    }
 
     public static DialogueManager GetInstance()
     {
         return instance;
     }
 
-    private void Start()
+    public void Start()
     {
+        //TypeText("This is some text comming from start", dialogueText);
+
         dialogueIsPlaying = false;
         dialoguePanel.SetActive(false);
 
@@ -60,7 +64,7 @@ public class DialogueManager : MonoBehaviour
         if (!dialogueIsPlaying)
         {
             return;
-        } 
+        }
 
         if (currentStory.currentChoices.Count == 0 && Input.GetKeyDown("space"))
         {
@@ -94,7 +98,13 @@ public class DialogueManager : MonoBehaviour
         if (currentStory.canContinue)
         {
             // set text for the current diaglogue line 
-            dialogueText.text = currentStory.Continue();
+            //dialogueText.text = currentStory.Continue();
+
+            // This applies the replacement above by using a typwriter function
+            string textToType = currentStory.Continue();
+            // use the string textToType and start tying it to dialogueText (inside the box)
+            StartCoroutine(TypeText(textToType, dialogueText));
+            
             // display choices, if any, for this dialogue line
             displayChoices();
         }
@@ -104,6 +114,34 @@ public class DialogueManager : MonoBehaviour
 
         }
     }
+
+    private IEnumerator TypeText(string textToType, TextMeshProUGUI dialogueText)
+    {
+
+        dialogueText.text = string.Empty;
+
+        //yield return new WaitForSeconds(1);
+
+        float t = 0;
+        int charIndex = 0;
+
+        while (charIndex < textToType.Length)
+        {
+
+
+            t += Time.deltaTime * typeWriterSpeed;
+            charIndex = Mathf.FloorToInt(t);
+            charIndex = Mathf.Clamp(charIndex, 0, textToType.Length);
+
+            dialogueText.text = textToType.Substring(0, charIndex);
+
+            yield return null;
+
+        }
+
+        dialogueText.text = textToType;
+    }
+
 
     private void displayChoices()
     {
