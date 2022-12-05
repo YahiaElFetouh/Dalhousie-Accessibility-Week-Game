5e5594d4bccf31a6b429dd63af3eb19a1f36969c
Ayah Abaza <ayah.abaza@dal.ca>
2022-12-05 16:09:02 -0400

    completed accessiblity option Cursor Resizing

--- Assets/Scripts/CursorSize.cs
+++ Assets/Scripts/CursorSize.cs
@@ -1,15 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CursorSize : MonoBehaviour
 {
     public Texture2D smallCursor;
     public Texture2D bigCursor;
-     
+    public Texture2D regCursor;
+    [SerializeField] Slider cursorSlider;
+
+    void Start(){
+        cursorReg();
+    }
+
+    public void changeCursor()
+    {
+        if(cursorSlider.value == 0){
+            cursorReduce();
+        }
+        else if (cursorSlider.value == 1){
+            cursorReg();
+        }
+        else if (cursorSlider.value == 2){
+            cursorEnlarge();
+        }
+        
+    }
+
     void cursorReduce()
     {
         Cursor.SetCursor(smallCursor, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    void cursorReg()
+    {
+        Cursor.SetCursor(regCursor, Vector2.zero, CursorMode.ForceSoftware);
     }
 
     void cursorEnlarge()
