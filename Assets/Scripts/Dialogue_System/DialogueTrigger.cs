8def49a1e25f8d0d899ce239ca461ddc558a3a1e
Joel Kuruvilla <jl567056@dal.ca>
2022-12-08 03:09:27 -0400

    Level start UI Implemented

--- Assets/Scripts/Dialogue_System/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue_System/DialogueTrigger.cs
@@ -5,39 +5,41 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     [Header("Visual Cue")]
-    [SerializeField] private GameObject visualCue;
+    [SerializeField] private GameObject dialogueCue;
 
     [Header("Ink JSON")]
     [SerializeField] private TextAsset inkJSON;
 
     private bool playerInRange;
+    private bool dialogueActive;
 
     private void Awake()
     {
         playerInRange = true; //OG: Was originally set to false.
-        visualCue.SetActive(true);
+        dialogueCue.SetActive(true);
 
+        dialogueActive = false;
     }
 
     private void Update()
     {
-        if (playerInRange)
+        if (playerInRange && !dialogueActive)
         {
-            visualCue.SetActive(true);
-
+            dialogueCue.SetActive(true);
             //Debug.Log(inkJSON.text);
-            
-            //
+
             if (Input.GetKeyDown("i"))
-            {
+            {     
                 //Debug.Log(inkJSON.text);
+                dialogueCue.SetActive(false);
+                dialogueActive = true;
                 DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
+            } else {
+                return;
             }
-            
         } else
         {
-            visualCue.SetActive(false);
-           
+            return;
         }
     }
 
