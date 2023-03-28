16e0ba6b300cf0e6b03290fd76df1d949d443cfb
Keenan <kn518475@dal.ca>
2023-03-28 10:35:54 -0300

    Saving state across scenes. To review my Yisong

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour{   
@@ -8,6 +9,7 @@ public class MainMenu : MonoBehaviour{
    public CursorSize cursorSettings;
    public MusicVolume musicVolumeSettings;
    public SoundManager musicToggleSettings;
+   public FilterDropdown filter;
 
 
    // Start is called before the first frame update
@@ -17,7 +19,14 @@ public class MainMenu : MonoBehaviour{
       cursorSettings.cursorSizeStatus();
       musicVolumeSettings.audioVolumeStatus();
       musicToggleSettings.musicToggleStatus();
+
+      //Keenan added this
+      filter.dropdownStatus();
    }
+
+
+
+
 
 
    /* Play the game from beginning to end */
