efc736c2113b992b97a8c0e5c28d5c0376638de8
Zephyr Xyd Espino <zp270900@dal.ca>
2023-03-29 12:40:02 -0300

    Revert "Merge branch 'Settings/CPStory/Test' of..."
    This reverts commit c9200c2ada01abe808bea31c3cbc3957af1a152e

--- Assets/Scripts/Settings/FilterDropdown.cs
+++ Assets/Scripts/Settings/FilterDropdown.cs
@@ -3,47 +3,27 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
+//YouTube. (2020). Unity Ui Tutorial Dropdown C# Scripting. YouTube. 
+//Retrieved March 1, 2023, from https://www.youtube.com/watch?v=URS9A4V_yLc. 
+
+//Also thanks for the help from CSCIx691 TA: Anay Awasthi
+
+
 public class FilterDropdown : MonoBehaviour
 {
-    private int selectedOption = 0; // Initialize the selected option to 0.
-    private static int holdOption;
-
+    
     void Start()
     {
         //get the value from drop down, it's like 0, 1, 2, 3
         var dropdown = transform.GetComponent<Dropdown>();
         
-        // Retrieve the selected option from PlayerPrefs if it exists.
-        if (PlayerPrefs.HasKey("selectedOption"))
-        {
-            selectedOption = PlayerPrefs.GetInt("selectedOption");
-            dropdown.value = selectedOption;
-        }
-
         //make change for dropdown by different choice, detail in function :void DropdownItemSelected(Dropdown d)
         DropdownItemSelected(dropdown);
-        holdOption = dropdown.value;
+        
         //apply the change 
         dropdown.onValueChanged.AddListener(delegate {DropdownItemSelected(dropdown);});
+        
     }
-    //New method from keenan
-    public void dropdownStatus()
-    {
-        //the colorblind is apply on the main camera
-        Camera myCamera=Camera.main;
-
-        var x=myCamera.GetComponent<Wilberforce.Colorblind>();
-
-        // Retrieve the selected option from PlayerPrefs if it exists.
-        if (PlayerPrefs.HasKey("selectedOption"))
-        {
-            selectedOption = PlayerPrefs.GetInt("selectedOption");
-            x.Type=selectedOption;
-        }
-
-        PlayerPrefs.SetInt("selectedOption", 2);
-    }
-
 
 
     void DropdownItemSelected(Dropdown d){
@@ -57,11 +37,7 @@ public class FilterDropdown : MonoBehaviour
         //the type in colorblind script is by number0, 1, 2, 3, 
         //so apply the numbers to dropdown after the dropdown choice changes
         x.Type=d.value;
-
-        holdOption = d.value;
         
-
-        PlayerPrefs.SetInt("selectedOption", holdOption);
     }
 
     
